113e7a4eb9fe13c37252f96f2a2e6f39bdd283fa
konstantin <konst.gromov@gmail.com>
2026-06-17 02:09:46 +0300

    Finish DDL - schema migration

--- src/main/resources/db/changelog/V001_1__Create_tables.sql
+++ src/main/resources/db/changelog/V001_1__Create_tables.sql
@@ -1,19 +1,5 @@
 -- V001_1__Create_tables.sql
-drop table if exists city;
-
-CREATE TABLE IF NOT EXISTS city (
-                                    ID          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                    Name        VARCHAR(35) NOT NULL,
-                                    CountryCode VARCHAR(3)  NOT NULL,
-                                    District    VARCHAR(20) NOT NULL,
-                                    Population  INT UNSIGNED DEFAULT 0 NOT NULL,
-                                    FOREIGN KEY (CountryCode) REFERENCES country(Code),
-                                    INDEX(CountryCode)
-    ) engine=InnoDB;
-
-
 drop table if exists country;
-
 CREATE TABLE IF NOT EXISTS country (
                                        Code             VARCHAR(3) NOT NULL PRIMARY KEY,
                                        Name             VARCHAR(60) NOT NULL,
@@ -32,9 +18,19 @@ CREATE TABLE IF NOT EXISTS country (
                                        Code2            VARCHAR(2) NOT NULL
 ) engine=InnoDB;
 
+drop table if exists city;
+CREATE TABLE IF NOT EXISTS city (
+                                    ID          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                    Name        VARCHAR(35) NOT NULL,
+                                    CountryCode VARCHAR(3)  NOT NULL,
+                                    District    VARCHAR(20) NOT NULL,
+                                    Population  INT UNSIGNED DEFAULT 0 NOT NULL,
+                                    FOREIGN KEY (CountryCode) REFERENCES country(Code),
+                                    INDEX(CountryCode)
+    ) engine=InnoDB;
+
 
 drop table if exists countrylanguage;
-
 CREATE TABLE IF NOT EXISTS countrylanguage (
                                                CountryCode VARCHAR(3)   NOT NULL,
                                                Language    VARCHAR(30)  NOT NULL,
